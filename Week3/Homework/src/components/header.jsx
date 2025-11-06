c4c61993d4c2fcb20282a4e2507554f99dbf1927
twossu <nadao202@naver.com>
2025-11-06 15:56:28 +0900

    build: tailwindCSS 설치설정

---
+++ Week3/Homework/src/components/header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="md:box-borde box-content bg-green-50">
+      <div className="text-lg font-bold">숫자 카드 짝 맞추기</div>
+    </div>
+  );
+};
+
+export default Header;
