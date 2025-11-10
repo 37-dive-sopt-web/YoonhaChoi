29143ef66f669fca01e6ba96bb7ab81f679ca3de
twossu <nadao202@naver.com>
2025-11-10 16:07:43 +0900

    feat: 포켓몬 실습

--- week4/practice/poketmon/src/pages/home.jsx
+++ week4/practice/poketmon/src/pages/home.jsx
@@ -1,12 +1,31 @@
-import { Link } from "react-router";
-
+import { useEffect, useState } from "react";
+import PokemonCard from "../components/pockemon-card";
+import axios from "axios";
 const Home = () => {
+  const [pockemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=40"
+        );
+        setPokemonList(res.data.results);
+      } catch (error) {
+        console.error("포켓몬 리스트를 불러오는 데 실패했습니다.", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div style={{ padding: "2rem" }}>
       <h1>포켓몬 도감</h1>
       <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem" }}>
-        <Link to="/pokemon/피카츄">피카츄</Link>
-        <Link to="/pokemon/이상해씨">이상해씨</Link>
+        {pockemonList.map((pokemon) => (
+          <PokemonCard key={pokemon.name} name={pokemon.name} />
+        ))}
       </div>
     </div>
   );
