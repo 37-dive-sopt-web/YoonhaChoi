e1c8488eb40625ad165253490e6db3ad29dd74e7
twossu <nadao202@naver.com>
2025-11-18 01:33:03 +0900

    feat: 버튼 컴포넌트 UI

--- week4/homework/src/components/button/button.tsx
+++ week4/homework/src/components/button/button.tsx
@@ -5,12 +5,20 @@ interface ButtonProps {
 }
 
 const Button = ({ children, disabled, onClick }: ButtonProps) => {
+  const baseStyle =
+    "h-10 w-full rounded-xl text-white font-bold transition-colors duration-200";
+
+  const falseStyle = "bg-teal-200/50 cursor-not-allowed";
+  const imageActiveClasses = "bg-teal-400 hover:bg-teal-500";
+
+  const stateClasses = disabled ? falseStyle : imageActiveClasses;
+
   return (
     <button
       type="button"
       disabled={disabled}
       onClick={onClick}
-      className="w-full"
+      className={`${baseStyle} ${stateClasses}`}
     >
       {children}
     </button>
