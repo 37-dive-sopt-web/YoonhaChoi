4fded985623c0de3c2fc51b5419387e8c92a73df
twossu <nadao202@naver.com>
2025-11-18 16:58:33 +0900

    fix: global.css.ts 삭제

--- week4/homework/src/styles/global.css.ts
+++
@@ -1,9 +0,0 @@
-import { globalStyle, style } from "@vanilla-extract/css";
-import { themeVars } from "./theme.css";
-
-globalStyle("html, body", {
-  fontSize: "62.5%",
-  fontFamily: `'Pretendard Variable', sans-serif`,
-});
-
-export const rootStyle = style({ backgroundColor: themeVars.color.white });
