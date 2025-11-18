7c1232df2b7c9f594723d6a2ca070d7e2b2998db
twossu <nadao202@naver.com>
2025-11-19 02:18:02 +0900

    fix: 로그인 컨테이너 패딩 수정

--- week4/homework/src/pages/login/login.css.ts
+++ week4/homework/src/pages/login/login.css.ts
@@ -6,7 +6,7 @@ export const container = style({
     height: '100vh',
     flexDirection: 'column',
     justifyContent: 'center',
-    padding:'3rem 45rem',
+    padding:'3rem 30rem',
 })
 
 export const title = style({
