22d3b9b905859c0b9cc90a8d5093ebf60563746b
twossu <nadao202@naver.com>
2025-11-19 00:07:59 +0900

    feat: 정보 수 api

--- week4/homework/src/pages/mypage/mypage.tsx
+++ week4/homework/src/pages/mypage/mypage.tsx
@@ -1,18 +1,57 @@
 import Button from "../../components/button/button";
 import Input from "../../components/input/input";
+import { useMyPage } from "../../hooks/use-mypage"; 
+import * as styles from "./mypage.css"; 
+
 
 const MyPage = () => {
-  return (
-    <div>
-     <p>내 정보</p>
-     <p>아이디</p>
-     <p>ㅁㄴㅇㄴ</p>
-     <Input label="이름" placeholder="이름을 입력해주세요." />
-     <Input label="이메일" placeholder="이메일을 입력해주세요." />
-     <Input label="나이" placeholder="나이를 입력해주세요." />
+  const {
+    formData,
+    isDisabled,
+    handleChange,
+    handleAgeChange,
+    handleSave,
+  } = useMyPage();
 
-     <Button children="수정하기" disabled onClick={()=>{}}/>
-    </div>
+  return (
+    <>
+      <h1 className={styles.title}>내 정보</h1>
+      
+      <div className={styles.idContainer}>
+          <label className={styles.label}>아이디</label>
+          <p className={styles.name}>
+             {formData.username}
+          </p>
+      </div>
+      
+      <Input 
+        label="이름" 
+        placeholder="이름을 입력해주세요." 
+        value={formData.name}
+        onChange={(e) => handleChange(e, 'name')}
+      />
+      
+      <Input 
+        label="이메일" 
+        placeholder="이메일을 입력해주세요." 
+        value={formData.email || ''}
+        onChange={(e) => handleChange(e, 'email')}
+      />
+      
+      <Input 
+        label="나이" 
+        placeholder="나이를 입력해주세요." 
+        value={formData.age === undefined || formData.age === null ? '' : String(formData.age)}
+        onChange={handleAgeChange}
+        type="number" 
+      />
+
+      <Button 
+        children="저장"
+        disabled={isDisabled} 
+        onClick={handleSave}
+      />
+    </>
   );
 };
 
